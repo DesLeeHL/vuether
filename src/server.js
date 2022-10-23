67e80668bb94b3ae8db5bb1f29941735491c7a36
DesLeeHL <lih4@tcd.ie>
2022-10-23 22:53:01 +0100

    updated

--- src/server.js
+++ src/server.js
@@ -1,18 +1,81 @@
-import dotenv  from "dotenv"
-import axios  from "axios"
+import dotenv from "dotenv"
+import axios from "axios"
 import express from "express"
-import path  from "path"
+import path from "path"
 
 dotenv.config()
 
-const app = express();
+const app = express()
 const port = 3000
 const API_key = process.env.OPENWEATHER_API_KEY
 const URL_base = 'https://api.openweathermap.org/data/2.5'
 
-
+const average = arr => arr.reduce((p, c) => p + c, 0) / arr.length
+const sum = arr => arr.reduce((p, c) => p + c, 0)
+const kel_to_cel = k => Math.round((k - 273.12) * 100) / 100
+const min_max = (arr) => {
+    const min = kel_to_cel(Math.min(...arr))
+    const max = kel_to_cel(Math.max(...arr))
+    return { min: min, max: max }
+}
 
 const city = 'Dublin'
+
+app.get('/', (req, res) => res.send('Vuether Server'))
+app.get('/forecast/:town', getForecast)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+function getMaskAdvise(forecastData) {
+    let pm2_5 = []
+    for (dateEntry in forecastData) {
+        pm2_5.push(forecastData[dateEntry].averagePM2_5)
+    }
+    // console.log(pm2_5)
+    const pm2_5_avg = average(pm2_5)
+    return pm2_5_avg > 10
+}
+
+function getTemperaturesSummary(forecastData) {
+    let max = 0
+    let min = forecastData[Object.keys(forecastData)[0]].averageTemp
+    let sentiment = null
+
+    let minMaxObj = {}
+
+    // Loop over every day getting the absolute min and max values
+    for (dateEntry in forecastData) {
+        minMaxObj = forecastData[dateEntry].temperatureRange
+
+        // Check if the max on this day is more than current max
+        if (minMaxObj.max >= max)
+            max = minMaxObj.max
+
+        // Check if the min on this day is more than current max
+        if (minMaxObj.min <= min)
+            min = minMaxObj.min
+
+    }
+
+
+
+    if (max >= 20.0)
+        sentiment = "hot"
+
+    else if (max <= 20.0 && min >= 10.0)
+        sentiment = "warm"
+
+    else
+        sentiment = "cold"
+
+
+    return {
+        sentiment: sentiment,
+        max: max,
+        min: min
+    }
+
+}
 
 axios.get(`${URL_base}/forecast?q=${city}&APPID=${API_key}`).then((response) => {
     console.log(`Forecast in ${city}`)
@@ -21,23 +84,105 @@ axios.get(`${URL_base}/forecast?q=${city}&APPID=${API_key}`).then((response) => 
 
 function getForcast(req, res) {
     let city = req.params.city
-    console.log(`Getting weather forecast for ${city}`)
+    console.log(`Getting weather forecast for ${city} ...`)
 
-    let forcastSumm = {}
+    let forcastData = {}
     let willRain = false
     let forecastSentiment = null
+    let airPolData = {}
+    var lat, long = 0
 
     axios.get(`${URL_base}/forecast?q=${city}&APPID=${API_key}`).then(
         (response) => {
-            res.json({
-                data: response.data
-            })
+            const { lat, lon } = response.data.city.coord
+            lati = lat
+            longi = lon
+            // const airPollution = getAirPollutionForecast(lat, lon)
+
+            var weatherData = response.data.list
+
+            //  Loop over OpenWeather API response and extract data for each day
+            for (weatherEntry in weatherData) {
+                // Make the date look nicer for front-end
+                let date = new Date(response.data.list[weatherEntry].dt * 1000)
+                date.setHours(0, 0, 0, 0)
+                date = date.toLocaleDateString()
+
+                // First check if there is a date entry for the given date, if not create one
+                if (!forecastSummary[date]) {
+                    forecastSummary[date] = {
+                        temperatures: [],
+                        windSpeeds: [],
+                        rainfallLevels: [],
+                    }
+                }
+
+                // Extract temperature and wind speed data
+                forecastSummary[date].temperatures.push(weatherData[weatherEntry].main.temp)
+                forecastSummary[date].windSpeeds.push(weatherData[weatherEntry].wind.speed)
+
+                // Check if there is any rain
+                if (weatherData[weatherEntry].rain && weatherData[weatherEntry].rain['3h']) {
+                    isRain = true
+                    forecastSummary[date].rainfallLevels.push(weatherData[weatherEntry].rain['3h'])
+                }
+
+            }
         }
     ).catch((error) => {
-        console.log(error);
-        res.status(400);
+        console.log(error)
+        res.status(400)
         res.json({
             error: "Bad Request!"
-        });
+        })
+    })
+
+    axios.get(`${base_url}/air_pollution/forecast?lat=${lati}&lon=${longi}&APPID=${API_key}`).then((response1) => {
+        const airPollutionData = response1.data.list
+        for (airPollutionEntry of airPollutionData) {
+            let date = new Date(airPollutionEntry.dt * 1000)
+            date.setHours(0, 0, 0, 0)
+            date = date.toLocaleDateString()
+
+            // First check if there is a date entry for the given date, if not create one
+            if (!airPollutionSummary[date]) {
+                airPollutionSummary[date] = {
+                    pm2_5: []
+                }
+            }
+            // Extract temperature and wind speed data
+            airPollutionSummary[date].pm2_5.push(airPollutionEntry.components.pm2_5)
+            // console.log(`${date} - PM2_5 - ${airPollutionEntry.components.pm2_5}`)
+        }
+
+        // When finished extracting data, calculate averages
+        for (dateEntry in forecastSummary) {
+            forecastSummary[dateEntry].averageTemp = kelvin_to_celsius(average(forecastSummary[dateEntry].temperatures))
+            forecastSummary[dateEntry].averageWind = average(forecastSummary[dateEntry].windSpeeds)
+            forecastSummary[dateEntry].rainfallLevels = sum(forecastSummary[dateEntry].rainfallLevels)
+            forecastSummary[dateEntry].temperatureRange = min_max(forecastSummary[dateEntry].temperatures)
+            forecastSummary[dateEntry].averagePM2_5 = average(airPollutionSummary[dateEntry].pm2_5)
+        }
+
+        // Get overall temperature sentiment
+        temperatureSummary = getTemperaturesSummary(forecastSummary)
+        maskAdvised = getMaskAdvise(forecastSummary)
+
+
+
+        res.json({
+            forecastSummary: forecastSummary,
+            isRain: isRain,
+            temperatureSummary: temperatureSummary,
+            maskAdvised: maskAdvised
+        })
+
+
+    }).catch((error) => {
+        console.error(error)
+        res.status(400)
+        res.json({
+            error: "Bad Request!"
+        })
     })
 }
