6b19dd4c618260f29b4f590b7debea59f2cf36ca
DesLeeHL <lih4@tcd.ie>
2022-10-23 22:13:31 +0100

    require to import

--- src/server.js
+++ src/server.js
@@ -8,13 +8,13 @@ dotenv.config()
 const app = express();
 const port = 3000
 const API_key = process.env.OPENWEATHER_API_KEY
-const url = 'https://api.openweathermap.org/data/2.5'
+const URL_base = 'https://api.openweathermap.org/data/2.5'
 
 
 
 const city = 'Dublin'
 
-axios.get(`${url}/forecast?q=${city}&APPID=${API_key}`).then((response) => {
+axios.get(`${URL_base}/forecast?q=${city}&APPID=${API_key}`).then((response) => {
     console.log(`Forecast in ${city}`)
     console.log(response.data)
 })
@@ -27,7 +27,7 @@ function getForcast(req, res) {
     let willRain = false
     let forecastSentiment = null
 
-    axios.get(`${base_url}/forecast?q=${city}&APPID=${API_key}`).then(
+    axios.get(`${URL_base}/forecast?q=${city}&APPID=${API_key}`).then(
         (response) => {
             res.json({
                 data: response.data
